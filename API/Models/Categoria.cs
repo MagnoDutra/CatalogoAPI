9a0e31b56854201194003dc2e286e2eb195f2891
Magno <jmagno.dutra@hotmail.com>
2025-01-06 01:19:51 -0300

    Ajustando tabela e adicionando migrations

--- API/Models/Categoria.cs
+++ API/Models/Categoria.cs
@@ -1,12 +1,20 @@
 using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace API.Models;
 
 public class Categoria
 {
   public int CategoriaId { get; set; }
-  public string? Nome { get; set; }
-  public string? ImagemUrl { get; set; }
+
+  [Required]
+  [StringLength(80)]
+  public required string Nome { get; set; }
+
+
+  [Required]
+  [StringLength(300)]
+  public required string ImagemUrl { get; set; }
 
   public ICollection<Produto>? Produtos { get; set; } = new Collection<Produto>();
 }
--- API/Models/Produto.cs
+++ API/Models/Produto.cs
@@ -1,12 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace API.Models;
 
+[Table("Produtos")]
 public class Produto
 {
+  [Key]
   public int ProdutoId { get; set; }
-  public string? Nome { get; set; }
-  public string? Descricao { get; set; }
-  public decimal Preco { get; set; }
-  public string? ImagemUrl { get; set; }
+
+  [Required]
+  [StringLength(80)]
+  public required string Nome { get; set; }
+
+  [Required]
+  [StringLength(300)]
+  public required string Descricao { get; set; }
+
+  [Required]
+  [Column(TypeName = "decimal(10,2)")]
+  public required decimal Preco { get; set; }
+
+  [Required]
+  [StringLength(300)]
+  public required string ImagemUrl { get; set; }
+
   public float Estoque { get; set; }
   public DateTime DataCadastro { get; set; }
 
