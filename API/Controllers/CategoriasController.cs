ac76c2171890c06d1288c2dc3521583fe58a70eb
Magno <jmagno.dutra@hotmail.com>
2025-01-07 21:46:03 -0300

    Implementada rotas de Categorias

--- API/Controllers/CategoriasController.cs
+++ API/Controllers/CategoriasController.cs
@@ -2,6 +2,7 @@ using API.Context;
 using API.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
@@ -20,15 +21,60 @@ public class CategoriasController(AppDbContext context) : ControllerBase
     }
 
 
-    [HttpGet("{id:int}")]
+    [HttpGet("{id:int}", Name = "ObterCategoria")]
     public ActionResult<Categoria> GetCategoria(int id)
     {
         var categoria = context.Categorias.FirstOrDefault(cat => cat.CategoriaId == id);
 
         if (categoria is null) return NotFound();
 
-        return categoria;
+        return Ok(categoria);
+    }
+
+    // GET
+    [HttpGet("produtos")]
+    public ActionResult<IEnumerable<Categoria>> GetCategoriasProdutos()
+    {
+        var categorias = context.Categorias.Include(cat => cat.Produtos).ToList();
+
+        if (categorias is null) return NotFound();
+
+        return Ok(categorias);
     }
 
 
+    [HttpPost]
+    public ActionResult PostCategoria(Categoria categoria)
+    {
+        if (categoria is null) return BadRequest();
+
+        context.Categorias.Add(categoria);
+        context.SaveChanges();
+
+        return new CreatedAtRouteResult("ObterCategoria", new { id = categoria.CategoriaId }, categoria);
+    }
+
+    [HttpPut("{id:int}")]
+    public ActionResult PutCategoria(int id, Categoria categoria)
+    {
+        if (categoria.CategoriaId != id) return BadRequest();
+
+        context.Entry(categoria).State = EntityState.Modified;
+        context.SaveChanges();
+
+        return Ok(categoria);
+    }
+
+    [HttpDelete("{id:int}")]
+    public ActionResult<Categoria> DeleteCategoria(int id)
+    {
+        var categoria = context.Categorias.FirstOrDefault(cat => cat.CategoriaId == id);
+
+        if (categoria == null) return BadRequest();
+
+        context.Categorias.Remove(categoria);
+        context.SaveChanges();
+
+        return Ok(categoria);
+    }
 }
