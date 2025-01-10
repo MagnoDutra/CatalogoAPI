72b603e34f03240767891c5b0de8324abaad8621
Magno <jmagno.dutra@hotmail.com>
2025-01-09 21:35:21 -0300

    Teste de validacoes

--- API/Models/Produto.cs
+++ API/Models/Produto.cs
@@ -1,17 +1,19 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
+using API.Validations;
 
 namespace API.Models;
 
 [Table("Produtos")]
-public class Produto
+public class Produto : IValidatableObject
 {
   [Key]
   public int ProdutoId { get; set; }
 
   [Required]
   [StringLength(80)]
+  [Teste]
   public required string Nome { get; set; }
 
   [Required]
@@ -34,4 +36,15 @@ public class Produto
   [JsonIgnore]
   public Categoria? Categoria { get; set; }
 
+  public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+  {
+    if (!string.IsNullOrEmpty(this.Nome))
+    {
+      var primeiraLetra = this.Nome[0].ToString();
+      if (primeiraLetra != primeiraLetra.ToUpper())
+      {
+        yield return new ValidationResult("A primeira letra deve ser maiuscula", new[] { nameof(this.Nome) });
+      }
+    }
+  }
 }
---
+++ API/Validations/TesteAttribute.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Validations;
+
+public class TesteAttribute : ValidationAttribute
+{
+  protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+  {
+    if (value == null || string.IsNullOrEmpty(value.ToString()))
+    {
+      return ValidationResult.Success;
+    }
+
+    var primeiraLetra = value.ToString()[0].ToString();
+    if (primeiraLetra != primeiraLetra.ToUpper())
+    {
+      return new ValidationResult("A Primeira letra do nome tem que ser maiuscula");
+    }
+
+    return ValidationResult.Success;
+  }
+}
